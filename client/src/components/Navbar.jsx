1b440e0f2b4e09769ab86d29656c6955e0936e92
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-06 19:36:29 +0530

    client- npm i react-router-dom installed successfully.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <nav className='NavbarItems'>
+        <h1 className='navbar-logo'>ExploreX</h1>
+        <ul className='nav-menu'>
+            <li>
+                <Link></Link>
+            </li>
+        </ul>
+      </nav>
+    </>
   )
 }
 
