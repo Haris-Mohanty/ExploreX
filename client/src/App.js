88ad0e85960d7eb65bdb74653039344b60529808
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-06 19:46:04 +0530

    client- Font awesome cdn added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar />
+      <Routes>{/* <Route path="/" element={} /> */}</Routes>
+    </>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import './Navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-      <nav className='NavbarItems'>
-        <h1 className='navbar-logo'>ExploreX</h1>
-        <ul className='nav-menu'>
-            <li>
-                <Link></Link>
-            </li>
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">ExploreX</h1>
+        <ul className="nav-menu">
+          <li>
+            <Link><i className="fa-solid fa-house-user"></i> Home</Link>
+          </li>
         </ul>
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
