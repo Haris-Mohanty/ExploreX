fcf696fdefbf34911bb6fbc9bf6fabcfac282347
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-07 12:48:01 +0530

    client- src- components- Navbar.css- Show hamburger icon when on mobile device code fixed.

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 import "./Navbar.css";
 
 const Navbar = () => {
+  const [isIconClicked, setIsIconClicked] = useState(false);
+
+  //Handle hamburger icon
+  const handleIcon = () => {
+    setIsIconClicked(!isIconClicked);
+  };
   return (
     <>
       <nav className="NavbarItems">
         <h1 className="navbar-logo">ExploreX</h1>
 
-<div className="menu-icons"><i className="fas fa-bar"></i></div>
+        <div className="menu-icons" onClick={handleIcon}>
+          {isIconClicked ? (
+            <i className="fas fa-times" />
+          ) : (
+            <i className="fas fa-bars" />
+          )}
+        </div>
 
         <ul className="nav-menu">
           {MenuItems.map((items, index) => (
