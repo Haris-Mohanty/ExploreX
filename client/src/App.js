183ed24e3474c0f50ea24b28628330d104cc060c
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-21 13:15:11 +0530

    client- src- components- app.js- Routes add for all navmenu(home,about,service & contact) successfully.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Service from "./routes/Service";
+import Contact from "./routes/Contact";
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        {/* <Route path="/" element={} /> */}</Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </>
   );
 }
