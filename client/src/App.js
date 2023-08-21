c2d4ad829d00b2b65aba082054e58a4dd6e60ec5
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-21 14:04:08 +0530

    client- src- components- routes- Home.jsx- Code processing....

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Service from "./routes/Service";
@@ -8,7 +7,7 @@ import Contact from "./routes/Contact";
 function App() {
   return (
     <>
-      <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
--- client/src/routes/Home.jsx
+++ client/src/routes/Home.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import homeImg from "../Assets/12.jpg";
 
 const Home = () => {
   return (
     <>
-      <h1>This is Home Page.</h1>
+      <Navbar />
+      <Hero
+        cName="hero"
+        heroImg={homeImg}
+        title="Your Journey, Our Passion"
+        text="Discover More, Explore More, Be More with Explorex!"
+        btnText="Set Plan"
+        btnClass="show"
+        url="/"
+      />
     </>
   );
 };
