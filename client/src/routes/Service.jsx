2b0d2460ca1897452fb5d86060b460cd22eaa93b
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-22 12:12:50 +0530

    client- src- components- routes- Contact.jsx- Image added.

--- client/src/routes/Service.jsx
+++ client/src/routes/Service.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import serviceImg from "../Assets/.jpg";
+import serviceImg from "../Assets/1.jpg";
 
 const Service = () => {
   return (
