8e601f93b0ab02f31a9a6feaa98998d8361ff4bf
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-26 19:46:18 +0530

    client- src- components- Destination.jsx- Create a dynamic component for add destination.....

---
+++ client/src/components/Destination.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Destination = () => {
+  return (
+    <>
+      <div className="destination">
+        
+      </div>
+    </>
+  )
+}
+
+export default Destination
