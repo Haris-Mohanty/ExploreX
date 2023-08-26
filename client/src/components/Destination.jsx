ea78a76fe23108e6703533f66bd3a8a583908a27
Haris Mohanty <harismohanty8658@gmail.com>
2023-08-26 19:52:21 +0530

    client- src- components- Destination.jsx- Add heading and some tagline...

--- client/src/components/Destination.jsx
+++ client/src/components/Destination.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
-
+import React from "react";
+import "./Destination.css";
 
 const Destination = () => {
   return (
     <>
       <div className="destination">
-        
+        <h1>Popular Destination</h1>
+        <p>Where adventure awaits at every turn!</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Destination
+export default Destination;
