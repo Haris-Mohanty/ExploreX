50630e7feb63bcf448c0d38d1284435e23be558d
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-04 11:59:46 +0530

    client- src- components- Tour.jsx- Adding recent tours.....

---
+++ client/src/components/Tour.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./TourStyles.css";
+
+const Tour = () => {
+  return (
+    <>
+      <div className="tour">
+        <h1>Recent Tours</h1>
+        <p>Unveiling Our World, One Journey at a Time</p>
+      </div>
+    </>
+  );
+};
+
+export default Tour;
