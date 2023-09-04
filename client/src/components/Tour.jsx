e1602bd965941fb255d22c68b8d22f37b8859844
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-04 18:17:20 +0530

    client- src- components- TourStyles.css- Media query code fixed.

--- client/src/components/Tour.jsx
+++ client/src/components/Tour.jsx
@@ -3,14 +3,14 @@ import "./TourStyles.css";
 import TourData from "./TourData";
 import Tour1 from "../Assets/sydney.png";
 import Tour2 from "../Assets/rome.png";
-import Tour3 from "../Assets/8.jpg";
+import Tour3 from "../Assets/abu.png";
 
 const Tour = () => {
   return (
     <>
       <div className="tour d-flex justify-conetnt-center align-items-center flex-column">
         <h1 className="fw-bold">Recent Tours</h1>
-        <p className="font-sofias">
+        <p className="font-sofias tagline">
           Unveiling Our World, One Journey at a Time!
         </p>
         <div className="tourCard">
@@ -25,9 +25,9 @@ const Tour = () => {
             text="Discover ancient wonders, Renaissance art, and vibrant culture on our unforgettable Rome tour."
           />
           <TourData
-            image={Tour1}
-            heading="Rome Unveiled"
-            text="Discover ancient wonders, Renaissance art, and vibrant culture on our unforgettable Rome tour."
+            image={Tour3}
+            heading="Abu Dhabi Allure"
+            text="Experience the irresistible allure of Abu Dhabi: a blend of opulence, culture, and desert mystique in one unforgettable tour."
           />
         </div>
       </div>
