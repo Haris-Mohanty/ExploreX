34d340b330ad277e798a17a21d7f8dbea48fdb0d
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-04 18:20:36 +0530

    client- src- components- Footer.jsx- Add footer info code processing....

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./FooterStyles.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="top"></div>
+        <div className="bottom"></div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
