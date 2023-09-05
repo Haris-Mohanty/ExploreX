1f39bd85959f40169bcfc65eb0000e3f486d23b3
Haris Mohanty <harismohanty8658@gmail.com>
2023-09-05 12:51:15 +0530

    client- src- routes- Signup.jsx- Code fixed.

--- client/src/routes/SignUp.jsx
+++ client/src/routes/SignUp.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import serviceImg from "../Assets/1.png";
+import signup from "../Assets/122.jpg";
+import Footer from "../components/Footer";
+import Registration from "../components/Registration";
 
 const SignUp = () => {
   return (
     <>
       <Navbar />
-      <Hero
-        cName="hero-mid"
-        heroImg={serviceImg}
-        title="Signup"
-        btnClass="hide"
-      />
+      <Hero cName="hero-mid" heroImg={signup} title="SignUp" btnClass="hide" />
+      <Registration />
+      <Footer />
     </>
   );
 };
